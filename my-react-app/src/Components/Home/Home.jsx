950a386a49351718ef9e950447c750d3b3fadf54
Romaisa0157 <romaisa0157@gmail.com>
2024-06-10 17:31:48 +0500

    modified:   my-react-app/src/Components/Home/Home.jsx

--- my-react-app/src/Components/Home/Home.jsx
+++ my-react-app/src/Components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './home.css'
-
+import video from '../../Assets/Video.mp4'
 const Home = () => {
   return (
     <section className='home'>
       <div className='overlay'>
-        <video src=''></video>
+        <video src={video} type="video/mp4"></video>
       </div>
     </section>
   )
