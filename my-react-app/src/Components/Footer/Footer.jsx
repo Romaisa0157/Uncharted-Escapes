81eed79e4c0010a37b36aaf1ad33ff8382efa458
Romaisa0157 <romaisa0157@gmail.com>
2024-06-20 23:01:37 +0500

    modified:   my-react-app/src/App.js 	modified:   my-react-app/src/Components/Footer/Footer.jsx 	modified:   my-react-app/src/Components/Footer/footer.css 	modified:   my-react-app/src/Components/Footer/footer.css.map 	modified:   my-react-app/src/Components/Footer/footer.scss 	modified:   my-react-app/src/Components/Main/Main.jsx 	modified:   my-react-app/src/Components/Main/main.css 	modified:   my-react-app/src/Components/Main/main.css.map 	modified:   my-react-app/src/Components/Main/main.scss

--- my-react-app/src/Components/Footer/Footer.jsx
+++ my-react-app/src/Components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import video2 from '../../Assets/Video.mp4'
+import { FiSend } from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <section className='footer'>
+      <div className='videoDiv'>
+        <video src={video2} autoPlay loop muted typeof='video/mp4' />
+      </div>
+
+      <div className='secContent container'>
+        <div className='contactDiv flex'>
+          <div className='text'>
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel With Us</h2>
+          </div>
+
+          <div className='inputDiv flex'>
+            <input type='text' placeholder='Enter Email Address' />
+
+            <button className='btn flex' type='submit'>
+              SEND<FiSend className='icon' />
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </section >
+
   )
 }
 
--- my-react-app/src/Components/Main/Main.jsx
+++ my-react-app/src/Components/Main/Main.jsx
@@ -5,14 +5,14 @@ import { LuClipboardSignature } from "react-icons/lu";
 
 
 import img1 from '../../Assets/imgs/img(1).jpg'
-// import img2 from '../../Assets/imgs/img(2).jpg'
-// import img3 from '../../Assets/imgs/img(3).jpg'
-// import img4 from '../../Assets/imgs/img(4).jpg'
-// import img5 from '../../Assets/imgs/img(5).jpg'
-// import img6 from '../../Assets/imgs/img(6).jpg'
-// import img7  from '../../Assets/imgs/img(7).jpg'
-// import img8 from '../../Assets/imgs/img(8).jpg'
-// import img9 from '../../Assets/imgs/img(9).jpg'
+import img2 from '../../Assets/imgs/img(2).jpg'
+import img3 from '../../Assets/imgs/img(3).jpg'
+import img4 from '../../Assets/imgs/img(4).jpg'
+import img5 from '../../Assets/imgs/img(5).jpg'
+import img6 from '../../Assets/imgs/img(6).jpg'
+import img7 from '../../Assets/imgs/img(7).jpg'
+import img8 from '../../Assets/imgs/img(8).jpg'
+import img9 from '../../Assets/imgs/img(9).jpg'
 
 
 const Data = [
@@ -26,85 +26,85 @@ const Data = [
     description: 'Known for its stunning overwater bungalows and crystal-clear waters, Bora Bora offers an idyllic setting for a perfect romantic getaway. Visitors can enjoy water sports, luxury resorts, and breathtaking sunsets.',
   },
 
-  // {
-  //   id: 2,
-  //   imgSrc: img2,
-  //   destTitle: 'Machu Picchu',
-  //   location: 'Peru',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$600',
-  //   description: 'Nestled high in the Andes Mountains, Machu Picchu is an ancient Incan city surrounded by lush greenery. Its archaeological significance and panoramic views attract visitors from all over the world.',
-  // },
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$600',
+    description: 'Nestled high in the Andes Mountains, Machu Picchu is an ancient Incan city surrounded by lush greenery. Its archaeological significance and panoramic views attract visitors from all over the world.',
+  },
 
-  // {
-  //   id: 3,
-  //   imgSrc: img3,
-  //   destTitle: 'Great Barrier Reef',
-  //   location: 'Australia',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The Great Barrier Reef is a paradise for divers and snorkelers. With its vibrant coral reefs and diverse marine life, it’s a spectacular natural wonder that promises an unforgettable experience.',
-  // },
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: 'Great Barrier Reef',
+    location: 'Australia',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'The Great Barrier Reef is a paradise for divers and snorkelers. With its vibrant coral reefs and diverse marine life, it’s a spectacular natural wonder that promises an unforgettable experience.',
+  },
 
-  // {
-  //   id: 4,
-  //   imgSrc: img4,
-  //   destTitle: 'Santorini',
-  //   location: 'Greece',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$500',
-  //   description: 'Famous for its white-washed buildings and stunning sunsets, Santorini offers a unique blend of history, culture, and beautiful beaches. It’s a perfect destination for relaxation and exploration.',
-  // },
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: 'Santorini',
+    location: 'Greece',
+    grade: 'CULTURAL RELAX',
+    fees: '$500',
+    description: 'Famous for its white-washed buildings and stunning sunsets, Santorini offers a unique blend of history, culture, and beautiful beaches. It’s a perfect destination for relaxation and exploration.',
+  },
 
-  // {
-  //   id: 5,
-  //   imgSrc: img5,
-  //   destTitle: 'Banff National Park',
-  //   location: 'Canada',
-  //   grade: 'ADVENTURE NATURE',
-  //   fees: '$400',
-  //   description: 'Banff National Park, located in the heart of the Canadian Rockies, is known for its stunning landscapes, including turquoise lakes, majestic mountains, and abundant wildlife. It’s a haven for outdoor enthusiasts.',
-  // },
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: 'Banff National Park',
+    location: 'Canada',
+    grade: 'ADVENTURE NATURE',
+    fees: '$400',
+    description: 'Banff National Park, located in the heart of the Canadian Rockies, is known for its stunning landscapes, including turquoise lakes, majestic mountains, and abundant wildlife. It’s a haven for outdoor enthusiasts.',
+  },
 
-  // {
-  //   id: 6,
-  //   imgSrc: img6,
-  //   destTitle: 'Kyoto',
-  //   location: 'Japan',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$650',
-  //   description: 'Kyoto, once the capital of Japan, is known for its classical Buddhist temples, as well as gardens, imperial palaces, Shinto shrines, and traditional wooden houses. It’s a city where ancient traditions meet modern life.',
-  // },
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: 'Kyoto',
+    location: 'Japan',
+    grade: 'CULTURAL RELAX',
+    fees: '$650',
+    description: 'Kyoto, once the capital of Japan, is known for its classical Buddhist temples, as well as gardens, imperial palaces, Shinto shrines, and traditional wooden houses. It’s a city where ancient traditions meet modern life.',
+  },
 
-  // {
-  //   id: 7,
-  //   imgSrc: img7,
-  //   destTitle: 'Cape Town',
-  //   location: 'South Africa',
-  //   grade: 'ADVENTURE NATURE',
-  //   fees: '$550',
-  //   description: 'Cape Town is renowned for its stunning landscapes, from the iconic Table Mountain to its beautiful beaches. The city offers a mix of adventure, culture, and vibrant nightlife.',
-  // },
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: 'Cape Town',
+    location: 'South Africa',
+    grade: 'ADVENTURE NATURE',
+    fees: '$550',
+    description: 'Cape Town is renowned for its stunning landscapes, from the iconic Table Mountain to its beautiful beaches. The city offers a mix of adventure, culture, and vibrant nightlife.',
+  },
 
-  // {
-  //   id: 8,
-  //   imgSrc: img8,
-  //   destTitle: 'Paris',
-  //   location: 'France',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$750',
-  //   description: 'Paris, the city of love, is known for its art, fashion, and culture. Visitors can explore historic landmarks like the Eiffel Tower and Louvre Museum, stroll along the Seine, and enjoy exquisite cuisine.',
-  // },
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: 'Paris',
+    location: 'France',
+    grade: 'CULTURAL RELAX',
+    fees: '$750',
+    description: 'Paris, the city of love, is known for its art, fashion, and culture. Visitors can explore historic landmarks like the Eiffel Tower and Louvre Museum, stroll along the Seine, and enjoy exquisite cuisine.',
+  },
 
-  // {
-  //   id: 9,
-  //   imgSrc: img9,
-  //   destTitle: 'Bali Island',
-  //   location: 'Indonesia',
-  //   grade: 'CULTURAL RELAX',
-  //   fees: '$500',
-  //   description: 'Bali is an Indonesian paradise known for its forested volcanic mountains, iconic rice paddies, beaches, and coral reefs. It’s a destination that offers a rich culture and stunning natural beauty.',
-  // },
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'CULTURAL RELAX',
+    fees: '$500',
+    description: 'Bali is an Indonesian paradise known for its forested volcanic mountains, iconic rice paddies, beaches, and coral reefs. It’s a destination that offers a rich culture and stunning natural beauty.',
+  },
 ];
 
 
@@ -138,22 +138,24 @@ const Main = () => {
                     <span className='name'>{location}</span>
                   </span>
 
-                  <div className='fees flex'>
-                    <div className='grade'>
+                  <div className='fees grid'>
+                    <div className="grade">
                       <span>{grade}<small>+1</small></span>
                     </div>
                     <div className='price'>
                       <h5>{fees}</h5>
                     </div>
-
-                    <div className='desc'>
-                      <p>{description}</p>
-                    </div>
-                    <button className='btn flex'>
-                      DETAILS
-                      <LuClipboardSignature className='icon' />
-                    </button>
                   </div>
+
+                  <div className='desc'>
+                    <p>{description}</p>
+                  </div>
+                  
+                  <button className='btn flex'>
+                    DETAILS
+                    <LuClipboardSignature className='icon' />
+                  </button>
+
                 </div>
               </div>
             )
